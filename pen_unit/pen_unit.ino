67fdb8e6bbaddd4bdef70e5a379e014f541e17ec
Jan-Tobias Matysik <jan-tobias.matysik@nexenio.com>
2020-03-22 15:39:29 +0100

    implement pen unit + left unit

--- pen_unit/pen_unit.ino
+++ pen_unit/pen_unit.ino
@@ -9,16 +9,16 @@
 #include <SPI.h>
 #include "RF24.h"
 #include "Shared.h"
-
+#define SERVO_PIN 8
+#define POWER_PIN 6
+#define ANGLE_DOWN 0
+#define ANGLE_UP 90
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins*/
 RF24 radio(10,9);
 Servo pen_servo;
-byte servoPin = 8;
-byte powerPin = 6;
 
-const byte angle_down = 0;
-const byte angle_up = 90;
+
 unsigned long power_time = 0;
 bool power_on = false;
 
@@ -26,63 +26,56 @@ Message msg_data;
 
 void setPen(PenPosition targetPos) {
   if (targetPos == UP) {
-    pen_servo.write(angle_up);
+    pen_servo.write(ANGLE_UP);
   }
   else {
-    pen_servo.write(angle_down);
+    pen_servo.write(ANGLE_DOWN);
   }
 }
 
 void setup() {
   Serial.begin(115200);
+  while(!Serial);
+
   Serial.println(F("Pen Unit"));
   
   radio.begin();
-
   radio.setPALevel(RF24_PA_LOW);
+  radio.setAutoAck(true);
+  radio.enableAckPayload();
+  radio.setRetries(0, 15);
+  radio.setPayloadSize(16);
   radio.openWritingPipe(addresses[MAIN_UNIT]);
-  radio.openReadingPipe(1,addresses[PEN_UNIT]);
+  radio.openReadingPipe(1, addresses[PEN_UNIT]);
   radio.startListening();
 
-  pen_servo.attach(servoPin);
-  pen_servo.write(angle_down);
+  pen_servo.attach(SERVO_PIN);
+  pen_servo.write(ANGLE_DOWN);
 
-  pinMode(powerPin, OUTPUT);
+  pinMode(POWER_PIN, OUTPUT);
 }
 
 void loop() {
   if( radio.available()){
-    radio.read( &msg_data, sizeof(msg_data) );             // Get the payload
-    radio.stopListening();                               // First, stop listening so we can talk  
+    //turn of burning energy
+    power_on = false;
+    digitalWrite(POWER_PIN, LOW);
 
-    //Send Nonce
-    radio.write( &msg_data.nonce, sizeof(msg_data.nonce) );
+    radio.read( &msg_data, sizeof(msg_data) );
 
-    Serial.print("Received");
-    Serial.println(msg_data.nonce);
+    Serial.println("Received: ");
+    Serial.println(msg_data.value);
+    Serial.println(msg_data.code);
     
-    setPen((PenPosition)msg_data.direction);
-    
-    for (byte i = 0; i < 1; i++) {
-      if (!radio.write( &msg_data.nonce, sizeof(msg_data.nonce) )){
-        Serial.print("Send failed ");
-        Serial.println(msg_data.nonce);
-        continue;
-      } else {
-        Serial.print("Sent response ");
-        Serial.println(msg_data.nonce);
-        break;
-      }
-    }
-    radio.startListening();
+    setPen((PenPosition)msg_data.code);
   }
   if (millis() - power_time > 10000) {
     power_time = millis();
     power_on = true;
-    digitalWrite(powerPin, HIGH);
+    digitalWrite(POWER_PIN, HIGH);
   }
-  else if (power_on && (millis() - power_time > 1000)) {
+  else if (power_on && (millis() - power_time > 500)) {
     power_on = false;
-    digitalWrite(powerPin, LOW);
+    digitalWrite(POWER_PIN, LOW);
   }
 }
--- right_unit/right_unit.ino
+++ right_unit/right_unit.ino
@@ -8,11 +8,18 @@
 #include <SPI.h>
 #include "RF24.h"
 #include "Shared.h"
+#include <TMC2208Stepper.h>
+
+#define IS_RIGHT_UNIT true
+#define STEP_PIN 5
+#define DIR_PIN 6
+#define EN_PIN 4
+#define MRES 4
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins*/
 RF24 radio(10,9);
-const byte directionPin = 6;
-const byte stepPin = 5;
+
+TMC2208Stepper driver = TMC2208Stepper(&Serial);	
 
 unsigned long parallel_step_count = 0;
 unsigned long total_steps = 0;
@@ -22,24 +29,54 @@ Message msg_data;
 void setup() {
 
   Serial.begin(115200);
-  Serial.println(F("Right Unit"));
+  while(!Serial);
+
+  #ifdef IS_RIGHT_UNIT
+    Serial.println(F("Right Unit"));
+  #else
+    Serial.println(F("Left Unit"));
+  #endif
+
+  //Setup Radio module
   
   radio.begin();
   radio.setPALevel(RF24_PA_LOW);
   radio.setAutoAck(true);
   radio.enableAckPayload();
-  radio.setRetries(0,15);
+  radio.setRetries(0, 15);
   radio.setPayloadSize(16);
+  driver.shaft(false);
 
   radio.openWritingPipe(addresses[MAIN_UNIT]);
-  radio.openReadingPipe(1,addresses[RIGHT_UNIT]);
+  #ifdef IS_RIGHT_UNIT
+    radio.openReadingPipe(1, addresses[RIGHT_UNIT]);
+  #else
+    radio.openReadingPipe(1, addresses[LEFT_UNIT]);
+    
+  #endif
   radio.startListening();
+
+  //Setup Driver module
+
+  pinMode(EN_PIN, OUTPUT);
+	pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+
+  driver.pdn_disable(1);         // Use PDN/UART pin for communication
+	driver.I_scale_analog(0);			 // Adjust current from the registers
+	driver.rms_current(500);			 // Set driver current 500mA
+  driver.intpol(true);           // enable x256 interpolation
+  driver.shaft(false);
+  driver.mres(MRES);             // set 1/2^4 microstepping
+	driver.toff(0x2);							 // Enable driver
+
+  digitalWrite(EN_PIN, LOW); //Enable driver
 }
 
 void controlMotor(unsigned long step_count, unsigned long sleep) {
-  for (unsigned long x = 1; x <= step_count; x++) {
-    digitalWrite(stepPin, HIGH);
-    digitalWrite(stepPin, LOW);
+  for (unsigned long x = 1; x <= step_count*pow(2, MRES); x++) {
+    digitalWrite(STEP_PIN, HIGH);
+    digitalWrite(STEP_PIN, LOW);
     delay(sleep);
     if (x % 5 == 0) Serial.print(".");
   }
@@ -48,9 +85,9 @@ void controlMotor(unsigned long step_count, unsigned long sleep) {
 
 void setDirection(Direction dir) {
   if (dir == BACKWARD) {
-    digitalWrite(directionPin, LOW);
+    driver.shaft(true);
   } else {
-    digitalWrite(directionPin, HIGH);
+    driver.shaft(false);
   }
 }
 
